2c388fd489ba91ab94653a88c533a96a2721b9ec
Sean Wheller <sean@prepaidmeters.com>
2019-07-29 14:01:07 +0200

    update depends

--- depends.sh
+++ depends.sh
@@ -8,7 +8,10 @@ echo "#################################"
 if [ $_APT_DEPENDS = 1 ]; then
 	
 	sudo apt install -y cpuid jo screen software-properties-common git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev gcc g++
-
+	# Install xmrig depends
+	sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
+	# Install xmrig-bash-scripts depends
+	sudo apt install jo cpuid
 else
         echo "Depend install is set to NO ($_APT_DEPENDS) Skipping"
 fi
