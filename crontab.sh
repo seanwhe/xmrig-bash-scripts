9b02099d8cdceb05b98ce09b2a8174a1464fa4be
Sean Wheller <sean@prepaidmeters.com>
2019-08-05 18:45:14 +0200

    changes to crontab

--- crontab.sh
+++ crontab.sh
@@ -14,12 +14,16 @@ crontab -l > crontab
 echo "Removing exiting crontab"
 crontab -r
 
+# Define env path
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/local/games:/snap/bin:/bin/:$_WORK_DIR
+
 # Setup users contab
 crontab -l | { cat; echo "#Start at $_USER_TIMESTART on days $_USER_CRONDAYS
-0 $_USER_CRONSTART * * $_USER_CRONDAYS $_WORK_DIR/start.sh"; } | crontab -
+0 $_USER_CRONSTART * * $_USER_CRONDAYS bash start.sh"; } | crontab -
 
 crontab -l | { cat; echo "#Stop at $_USER_TIMESTOP on days $_USER_CRONDAYS
-0 $_USER_CRONSTOP * * $_USER_CRONDAYS $_WORK_DIR/stop.sh"; } | crontab -
+0 $_USER_CRONSTOP * * $_USER_CRONDAYS bash stop.sh"; } | crontab -
+
 
 crontab -l
 
