dd97fdc694e7493c580528686f48b5d1cfce62cf
Sean Wheller <sean@prepaidmeters.com>
2019-08-12 19:50:08 +0200

    fix unary operator

--- depends.sh
+++ depends.sh
@@ -4,7 +4,7 @@
 
 echo "${_GREEN}INSTALL DEPENDS STARTED${_RESET}"
 
-if [ $_APT_DEPENDS = 1 ]; then
+if [ "$_APT_DEPENDS" -eq "1" ]; then
 
         # Install xmrig depends	
 	sudo apt install -y software-properties-common git build-essential cmake libuv1-dev libssl-dev libmicrohttpd-dev libhwloc-dev gcc g++
